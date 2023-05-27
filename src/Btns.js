253987a291d35a3ea96f32c86997db5416dc8fab
tubi55 <tubi55@nate.com>
2023-05-27 14:12:26 +0900

    prev버튼 클릭시 패널 회전

--- src/Btns.js
+++ src/Btns.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from 'react';
+
 function Btns({ frame }) {
+	let [Num, setNum] = useState(0);
+
+	useEffect(() => {
+		console.log(Num);
+	}, [Num]);
+
 	return (
 		<>
-			<button className='prev' onClick={() => console.log(frame)}>
+			<button
+				className='prev'
+				onClick={() => {
+					setNum(++Num);
+					frame.current.style.transform = `rotate(${45 * Num}deg)`;
+				}}
+			>
 				PREV
 			</button>
 			<button className='next' onClick={() => console.log(frame)}>
